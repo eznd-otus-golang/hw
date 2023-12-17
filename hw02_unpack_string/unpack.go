67c5c699b9a443a6cd19573b654ab54e3a9ae914
Evgeniy Zinder <evgeny.zinder@gmail.com>
2023-12-17 19:14:36 +0300

    wip

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -6,7 +6,51 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+const asciiZero = 48
+
+func Unpack(s string) (res string, err error) {
+	src := []rune(s)
+
+	inCounter := false
+	for pos := range src {
+		if inCounter {
+			inCounter = false
+			continue
+		}
+
+		if isDigit(src[pos]) {
+			err = ErrInvalidString
+			break
+		}
+
+		if pos == len(src)-1 {
+			res += string(src[pos])
+			break
+		}
+
+		if isDigit(src[pos+1]) {
+			if atoi(src[pos+1]) > 0 {
+				res += repeat(src[pos], atoi(src[pos+1]))
+			}
+			inCounter = true
+		} else {
+			res += string(src[pos])
+		}
+	}
+	return
+}
+
+func atoi(s rune) int {
+	return int(s) - asciiZero
+}
+
+func isDigit(s rune) bool {
+	return s >= asciiZero && s <= asciiZero+9
+}
+
+func repeat(s rune, n int) (res string) {
+	for i := 0; i < n; i++ {
+		res += string(s)
+	}
+	return
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,9 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		// + some utf-8 examples
+		{input: "ㄲㅜㄹㅂㅓㄹ", expected: "ㄲㅜㄹㅂㅓㄹ"},
+		{input: "ㄲㅜ2ㄹㅂㅓㄹ3", expected: "ㄲㅜㅜㄹㅂㅓㄹㄹㄹ"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
